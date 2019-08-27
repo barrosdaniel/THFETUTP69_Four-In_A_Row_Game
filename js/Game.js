47b71f8503cd046dc323e5463f175eed9c7480e7
barros.daniel@outlook.com <barros.daniel@outlook.com>
2019-08-27 22:12:44 +0800

    feature: Add interactivity methods to game

--- js/Game.js
+++ js/Game.js
@@ -4,34 +4,75 @@ class Game {
         this.players = this.createPlayers();
         this.ready = false;
     }
-    
-    
+
+
     /** 
      * Returns active player.
      * @return  {Object}    player - The active player.
      */
-	get activePlayer() {
+    get activePlayer() {
         return this.players.find(player => player.active);
-	}
-    
-    
+    }
+
+
     /** 
      * Creates two player objects
      * @return  {array}    An array of two player objects.
      */
     createPlayers() {
         const players = [new Player('Player 1', 1, '#e15258', true),
-                         new Player('Player 2', 2, '#e59a13')];
+            new Player('Player 2', 2, '#e59a13')
+        ];
         return players;
     }
-    
-    
+
     /** 
      * Begins game. 
      */
-    startGame(){
+    startGame() {
         this.board.drawHTMLBoard();
         this.activePlayer.activeToken.drawHTMLToken();
         this.ready = true;
+    }
+
+    /**
+     * Branches code, depending on what key player presses
+     * @param   {Object}    e - Keydown event object
+     */
+    handleKeydown(e) {
+        if (this.ready) {
+            if (e.key === 'ArrowLeft') {
+                // move token to the left
+                this.activePlayer.activeToken.moveLeft();
+            } else if (e.key === 'ArrowRight') {
+                // move token to the right
+                this.activePlayer.activeToken.moveRight(this.board.columns);
+            } else if (e.key === 'ArrowDown') {
+                // drop  token in the column
+                this.playToken();
+            }
+        }
+    }
+
+    /**
+     * Finds Space object to drop Token into, drops Token
+     */
+
+    playToken() {
+        let spaces = this.board.spaces;
+        let activeToken = this.activePlayer.activeToken;
+        let targetColumn = spaces[activeToken.columnLocation];
+        let targetSpace = null;
+
+        for (let space of targetColumn) {
+            if (space.token === null) {
+                targetSpace = space;
+            }
+        }
+
+        if (targetSpace !== null) {
+            game.ready = false;
+            activeToken.drop(targetSpace);
+        }
     }
 }
