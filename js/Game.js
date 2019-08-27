f3271b7d98059ecc8d1173425ade366f2d2e66d9
barros.daniel@outlook.com <barros.daniel@outlook.com>
2019-08-27 23:29:31 +0800

    feature: Add game handling methods

--- js/Game.js
+++ js/Game.js
@@ -57,7 +57,6 @@ class Game {
     /**
      * Finds Space object to drop Token into, drops Token
      */
-
     playToken() {
         let spaces = this.board.spaces;
         let activeToken = this.activePlayer.activeToken;
@@ -74,5 +73,84 @@ class Game {
             game.ready = false;
             activeToken.drop(targetSpace);
         }
+    }
+
+    /** 
+     * Checks if there a winner on the board after each token drop.
+     * @param   {Object}    Targeted space for dropped token.
+     * @return  {boolean}   Boolean value indicating whether the game has been won (true) or not (false)
+     */
+    checkForWin(target) {
+        const owner = target.token.owner;
+        let win = false;
+
+        // vertical
+        for (let x = 0; x < this.board.columns; x++) {
+            for (let y = 0; y < this.board.rows - 3; y++) {
+                if (this.board.spaces[x][y].owner === owner &&
+                    this.board.spaces[x][y + 1].owner === owner &&
+                    this.board.spaces[x][y + 2].owner === owner &&
+                    this.board.spaces[x][y + 3].owner === owner) {
+                    win = true;
+                }
+            }
+        }
+
+        // horizontal
+        for (let x = 0; x < this.board.columns - 3; x++) {
+            for (let y = 0; y < this.board.rows; y++) {
+                if (this.board.spaces[x][y].owner === owner &&
+                    this.board.spaces[x + 1][y].owner === owner &&
+                    this.board.spaces[x + 2][y].owner === owner &&
+                    this.board.spaces[x + 3][y].owner === owner) {
+                    win = true;
+                }
+            }
+        }
+
+        // diagonal
+        for (let x = 3; x < this.board.columns; x++) {
+            for (let y = 0; y < this.board.rows - 3; y++) {
+                if (this.board.spaces[x][y].owner === owner &&
+                    this.board.spaces[x - 1][y + 1].owner === owner &&
+                    this.board.spaces[x - 2][y + 2].owner === owner &&
+                    this.board.spaces[x - 3][y + 3].owner === owner) {
+                    win = true;
+                }
+            }
+        }
+
+        // diagonal
+        for (let x = 3; x < this.board.columns; x++) {
+            for (let y = 3; y < this.board.rows; y++) {
+                if (this.board.spaces[x][y].owner === owner &&
+                    this.board.spaces[x - 1][y - 1].owner === owner &&
+                    this.board.spaces[x - 2][y - 2].owner === owner &&
+                    this.board.spaces[x - 3][y - 3].owner === owner) {
+                    win = true;
+                }
+            }
+        }
+
+        return win;
+    }
+
+    /** 
+     * Switches active player. 
+     */
+    switchPlayers() {
+        for (let player of this.players) {
+            player.active = player.active === true ? false : true;
+        }
+    }
+
+    /** 
+     * Displays game over message.
+     * @param {string} message - Game over message.      
+     */
+    gameOver(message) {
+        const gameOverDiv = document.getElementById('game-over');
+        gameOverDiv.style.display = "block";
+        gameOverDiv.textContent = message;
     }
 }
